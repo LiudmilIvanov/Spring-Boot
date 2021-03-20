e2fe4bf4fd7de7ce48fbc3eecb4ddf41df559a94
pc <pc@DESKTOP-UOPF3HI>
2021-03-20 10:48:27 +0200

    Initial project created.

---
+++ judge-project/src/main/java/com/example/demo/JudgeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudgeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudgeProjectApplication.class, args);
+	}
+
+}
