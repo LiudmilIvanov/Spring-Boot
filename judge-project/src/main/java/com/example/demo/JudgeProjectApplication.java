42cb9bedd95e1006f587b43b35fdffcd4379ee07
pc <pc@DESKTOP-UOPF3HI>
2021-03-20 22:35:22 +0200

    project application updated

--- judge-project/src/main/java/com/example/demo/JudgeProjectApplication.java
+++ judge-project/src/main/java/com/example/demo/JudgeProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JudgeProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JudgeProjectApplication.class, args);
+		SpringApplication.run(JudgeProjectApplication.class, args); 
 	}
 
 }
